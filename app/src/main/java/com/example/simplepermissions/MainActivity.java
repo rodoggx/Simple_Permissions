eef1f766a514b156d8f6bd4b1339d558b6b7f455
rodoggx <mikerodolph@yahoo.com>
2016-08-11 16:52:32 -0400

    add doMagic commit 2

--- app/src/main/java/com/example/simplepermissions/MainActivity.java
+++ app/src/main/java/com/example/simplepermissions/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.simplepermissions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "doMagicTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doMagic(View view) {
-        File file = android.os.Environment.getExternalStorageState();
+        File file = android.os.Environment.getExternalStorageDirectory();
+        Log.d(TAG, "doMagic: " + file);
 
     }
 }
